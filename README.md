987368f5f228b5c6bc088e9eae95dfd152e3efd9
Meshach Griffith <meshachgriffith@gmail.com>
2021-08-12 16:50:45 -0400

    intial commit without webservices

--- README.md
+++ README.md
@@ -1,22 +1,3 @@
-# Welcome to CCPD3V-T3@M #
+#CCP Usermanagement
 
-Our operations team needs to create a basic project for the management of its users.
-
-### Requirements? ###
-
-* Angular (last version)
-* User must be able to Login, LogOut
-* Page CRUD for users (using api/token). 
-
-### Service (backend)  ###
-
-* API (ask for it)
-* Are you fullstack? So, use your own backend service ;)
-
-### Complete? ###
-
-* Create a new branch with the following name: [FrontEnd-Version-YourName]
-* Commit
-* Push to your branch
-* You can re-write .readme in your branch and explain how to set up your project.
-* Enjoy :)
+[View on StackBlitz ⚡️](https://angular-ivy-t2xyzj.stackblitz.io/login?returnUrl=%2F)
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+import { CrudComponent } from './crud/crud.component';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { AdminComponent } from './admin';
+import { LoginComponent } from './login';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    CrudComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
